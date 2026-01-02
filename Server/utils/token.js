b3186754d4f51cc211f81f98ecd7388889759978
PrethigahShanmugarajah <prethigah@gmail.com>
2026-01-02 19:53:19 +0530

    Add JWT token generation utility

---
+++ Server/utils/token.js
@@ -0,0 +1,15 @@
+// BiteRoute / Server / utils / token.js
+import jwt from "jsonwebtoken";
+
+/* -------- Generate JWT Token -------- */
+export const genToken = (userId) => {
+  try {
+    const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+      expiresIn: process.env.JWT_EXPIRES_IN,
+    });
+    return token;
+  } catch (error) {
+    console.log("JWT Generation Error:", error);
+    return null;
+  }
+};
