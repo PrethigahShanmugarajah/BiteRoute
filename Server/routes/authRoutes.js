54a85c9d1734b539af4ea3278bdb51c3abcb0ff3
PrethigahShanmugarajah <prethigah@gmail.com>
2026-01-02 20:35:25 +0530

    Add signIn route in authRoutes

--- Server/routes/authRoutes.js
+++ Server/routes/authRoutes.js
@@ -1,9 +1,10 @@
 // Cravely / Server / routes / authRoutes.js
 import express from "express";
-import { signUp } from "../controllers/authController.js";
+import { signIn, signUp } from "../controllers/authController.js";
 
 const authRouter = express.Router();
 
 authRouter.post("/signup", signUp);
+authRouter.post("/signin", signIn);
 
 export default authRouter;
