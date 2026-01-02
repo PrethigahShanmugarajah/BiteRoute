f3614d24be282fdd3d3ae388f2bbcc06ba7260f2
PrethigahShanmugarajah <prethigah@gmail.com>
2026-01-03 01:36:14 +0530

    Add capitalizeFirstLetter helper function

---
+++ Client/src/utils/helper.js
@@ -0,0 +1,7 @@
+// BiteRoute / Client / src / utils / helper.js
+
+/* -------- Capitalize first letter -------- */
+export const capitalizeFirstLetter = (str) => {
+  if (!str) return "";
+  return str.charAt(0).toUpperCase() + str.slice(1);
+};
