8c549275f57be9615767c0512472e9257383f50b
PrethigahShanmugarajah <prethigah@gmail.com>
2026-01-02 18:12:04 +0530

    Initial commit  Server Side

---
+++ Server/server.js
@@ -0,0 +1,23 @@
+// BiteRoute / Server / server.js
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+
+/* -------- INITIALIZE EXPRESS -------- */
+const app = express();
+
+/* -------- CONNECT TO DATABASE -------- */
+
+/* -------- MIDDLEWARE CONFIGURATION -------- */
+app.use(express.json());
+app.use(cors());
+
+/* -------- ROUTES -------- */
+app.get("/", (req, res) => res.send("API is Working!"));
+
+/* -------- PORT -------- */
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
