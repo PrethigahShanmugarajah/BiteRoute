b872568fa8b7db7f469797c63683d4a17434a61c
PrethigahShanmugarajah <prethigah@gmail.com>
2026-01-02 19:57:07 +0530

    Add authRouter

--- Server/server.js
+++ Server/server.js
@@ -3,6 +3,7 @@ import express from "express";
 import "dotenv/config";
 import cors from "cors";
 import connectDB from "./config/db.js";
+import authRouter from "./routes/authRoutes.js";
 
 /* -------- INITIALIZE EXPRESS -------- */
 const app = express();
@@ -12,10 +13,16 @@ await connectDB();
 
 /* -------- MIDDLEWARE CONFIGURATION -------- */
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
 
 /* -------- ROUTES -------- */
 app.get("/", (req, res) => res.send("API is Working!"));
+app.use("/api/auth", authRouter);
 
 /* -------- PORT -------- */
 const port = process.env.PORT || 5000;
