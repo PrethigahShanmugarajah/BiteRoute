3e3f7c6f8cb68f543c0ffe91483b5120cd4b3921
PrethigahShanmugarajah <prethigah@gmail.com>
2026-01-02 21:16:09 +0530

    Initial commit Client Side

---
+++ Client/src/App.jsx
@@ -0,0 +1,13 @@
+// BiteRoute / Client / src / App.jsx
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-primary bg-bg border border-border hover:bg-hover">
+        App
+      </h1>
+    </div>
+  );
+};
+
+export default App;
