eccb62b5370182f8388d8c848ef34d50e8a0cb1d
PrethigahShanmugarajah <prethigah@gmail.com>
2026-01-04 02:52:18 +0530

    Add OwnerDashboard component

---
+++ Client/src/components/Dashboards/OwnerDashboard.jsx
@@ -0,0 +1,11 @@
+// BiteRoute / Client / src / components / Dashboards / OwnerDashboard.jsx
+
+const OwnerDashboard = () => {
+  return (
+    <div>
+      <h1>OwnerDashboard</h1>
+    </div>
+  );
+};
+
+export default OwnerDashboard;
