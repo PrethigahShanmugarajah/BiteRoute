27a5ed04e671f73867c4ffd9f9281ebe0cf1b517
PrethigahShanmugarajah <prethigah@gmail.com>
2026-01-04 02:34:39 +0530

    Integrate useGetCurrentUser hook and conditional routing in App

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,18 +1,40 @@
 // BiteRoute / Client / src / App.jsx
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
 import { ToastContainer } from "react-toastify";
 import ForgotPassword from "./pages/ForgotPassword";
+import useGetCurrentUser from "./hooks/useGetCurrentUser";
+import { useSelector } from "react-redux";
+import Home from "./pages/Home";
 
 const App = () => {
+  useGetCurrentUser();
+  const { userData } = useSelector((state) => state.user);
+
   return (
     <>
       <ToastContainer />
       <Routes>
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route
+          path="/signup"
+          element={!userData ? <SignUp /> : <Navigate to={"/"} />}
+        />
+
+        <Route
+          path="/signin"
+          element={!userData ? <SignIn /> : <Navigate to={"/"} />}
+        />
+
+        <Route
+          path="/forgot-password"
+          element={!userData ? <ForgotPassword /> : <Navigate to={"/"} />}
+        />
+
+        <Route
+          path="/"
+          element={userData ? <Home /> : <Navigate to={"/signin"} />}
+        />
       </Routes>
     </>
   );
