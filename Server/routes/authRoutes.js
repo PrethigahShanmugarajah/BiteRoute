d54507348c633b508b5f7cb3c1f44b0f482c5fac
PrethigahShanmugarajah <prethigah@gmail.com>
2026-01-03 16:31:31 +0530

    Add sendOtp route in authRoutes

--- Server/routes/authRoutes.js
+++ Server/routes/authRoutes.js
@@ -1,11 +1,17 @@
 // Cravely / Server / routes / authRoutes.js
 import express from "express";
-import { signIn, signOut, signUp } from "../controllers/authController.js";
+import {
+  sendOtp,
+  signIn,
+  signOut,
+  signUp,
+} from "../controllers/authController.js";
 
 const authRouter = express.Router();
 
 authRouter.post("/signup", signUp);
 authRouter.post("/signin", signIn);
 authRouter.get("/signout", signOut);
+authRouter.post("/send-otp", sendOtp);
 
 export default authRouter;
