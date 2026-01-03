edb6d21d356ae0aafde284430d1bb5853d08a593
PrethigahShanmugarajah <prethigah@gmail.com>
2026-01-04 02:51:53 +0530

    Add DeliveryPerson dashboard component

---
+++ Client/src/components/Dashboards/DeliveryPerson.jsx
@@ -0,0 +1,11 @@
+// BiteRoute / Client / src / components / Dashboards / DeliveryPerson.jsx
+
+const DeliveryPerson = () => {
+  return (
+    <div>
+      <h1>DeliveryPerson</h1>
+    </div>
+  );
+};
+
+export default DeliveryPerson;
