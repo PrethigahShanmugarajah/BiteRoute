82398ed541eb219dda313e7ff7d02169ef952ed7
PrethigahShanmugarajah <prethigah@gmail.com>
2026-01-04 02:34:22 +0530

    Add useGetCurrentUser hook to fetch and set current user

---
+++ Client/src/hooks/useGetCurrentUser.jsx
@@ -0,0 +1,46 @@
+// BiteRoute / Client / src / hooks / useGetCurrentUser.jsx
+import { useEffect } from "react";
+import api from "../api/axios";
+import API_ROUTES from "../api/api_route";
+import { toast } from "react-toastify";
+import { setUserData } from "../redux/userSlice";
+import { useDispatch } from "react-redux";
+import { fetchUser } from "../services/fetch";
+
+function useGetCurrentUser() {
+  // useEffect(() => {
+  //   const fetchUser = async () => {
+  //     try {
+  //       const { data } = await api.get(API_ROUTES.USER.USER_GET, {
+  //         withCredentials: true,
+  //       });
+  //       console.log("Fetch User API Response:", data);
+  //       if (data.success) {
+  //         console.log("Fetch User Success:", data.message);
+  //         dispatch(setUserData(data.user));
+  //       } else {
+  //         toast.error(data.message);
+  //         console.log("Fetch User Data Error:", error);
+  //       }
+  //     } catch (error) {
+  //       toast.error(error?.response?.data?.message || error?.message);
+  //       console.log("Fetch User Error:", error);
+  //     }
+  //     fetchUser();
+  //   };
+  // }, []);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const fetchUserService = async () => {
+      const user = await fetchUser();
+      if (user) {
+        dispatch(setUserData(user));
+      }
+    };
+    fetchUserService();
+  }, [dispatch]);
+}
+
+export default useGetCurrentUser;
