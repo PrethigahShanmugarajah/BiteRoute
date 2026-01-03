7f1add45277630022a8ea4d119a79f13b3d8ba06
PrethigahShanmugarajah <prethigah@gmail.com>
2026-01-04 01:12:56 +0530

    Add userRouter

--- Server/server.js
+++ Server/server.js
@@ -2,8 +2,10 @@
 import express from "express";
 import "dotenv/config";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 import connectDB from "./config/db.js";
 import authRouter from "./routes/authRoutes.js";
+import userRouter from "./routes/userRoutes.js";
 
 /* -------- INITIALIZE EXPRESS -------- */
 const app = express();
@@ -13,6 +15,7 @@ await connectDB();
 
 /* -------- MIDDLEWARE CONFIGURATION -------- */
 app.use(express.json());
+app.use(cookieParser());
 app.use(
   cors({
     origin: process.env.FRONTEND_URL,
@@ -23,6 +26,7 @@ app.use(
 /* -------- ROUTES -------- */
 app.get("/", (req, res) => res.send("API is Working!"));
 app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
 
 /* -------- PORT -------- */
 const port = process.env.PORT || 5000;
