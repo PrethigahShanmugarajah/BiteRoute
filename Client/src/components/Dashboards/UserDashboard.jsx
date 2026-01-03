6a0b3f443d6a179905d085f4d2f173f7056dbe3d
PrethigahShanmugarajah <prethigah@gmail.com>
2026-01-04 02:52:27 +0530

    Add UserDashboard component

---
+++ Client/src/components/Dashboards/UserDashboard.jsx
@@ -0,0 +1,11 @@
+// BiteRoute / Client / src / components / Dashboards / UserDashboard.jsx
+
+const UserDashboard = () => {
+  return (
+    <div>
+      <h1>UserDashboard</h1>
+    </div>
+  );
+};
+
+export default UserDashboard;
