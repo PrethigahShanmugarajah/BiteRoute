e02ce8290eb8d5ad039c370fc1c86f9e2cdb9b13
PrethigahShanmugarajah <prethigah@gmail.com>
2026-01-04 20:33:16 +0530

    Add getMyShop route in shopRoutes

--- Server/routes/shopRoutes.js
+++ Server/routes/shopRoutes.js
@@ -1,11 +1,12 @@
 // Cravely / Server / routes / shopRoutes.js
 import express from "express";
 import { isAuth } from "../middlewares/isAuth.js";
-import { createEditShop } from "../controllers/shopController.js";
+import { createEditShop, getMyShop } from "../controllers/shopController.js";
 import upload from "../middlewares/multer.js";
 
 const shopRouter = express.Router();
 
 shopRouter.post("/create-edit", isAuth, upload.single("file"), createEditShop);
+shopRouter.get("/get-shop", isAuth, getMyShop);
 
 export default shopRouter;
