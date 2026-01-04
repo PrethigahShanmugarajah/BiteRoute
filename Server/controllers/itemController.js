a40ac66e198b291212c03620146ca9c2aaa1c9c4
PrethigahShanmugarajah <prethigah@gmail.com>
2026-01-04 19:17:28 +0530

    Create function for Update Item in itemController

--- Server/controllers/itemController.js
+++ Server/controllers/itemController.js
@@ -44,3 +44,49 @@ export const addItem = async (req, res) => {
     });
   }
 };
+
+/* -------- Update Item -------- */
+export const updateItem = async (req, res) => {
+  try {
+    const itemId = req.params.itemId;
+    const { name, category, foodType, price } = req.body;
+
+    let image;
+    if (req.file) {
+      image = await uploadOnCloudinary(req.file.path);
+    }
+
+    const item = await Item.findByIdAndUpdate(
+      itemId,
+      {
+        name,
+        category,
+        foodType,
+        price,
+        image,
+      },
+      { new: true }
+    );
+
+    if (!item) {
+      return res.status(404).json({
+        success: false,
+        message: "Item not found.",
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "Item updated successfully!",
+      item,
+    });
+  } catch (error) {
+    console.error("Update Item Error:", error.message);
+
+    return res.status(500).json({
+      success: false,
+      message: "Failed to Update Item.",
+      error: `Update Item Error: ${error.message}`,
+    });
+  }
+};
