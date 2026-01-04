82f5dcb9451d793a65a84f909edc26dc0502d903
PrethigahShanmugarajah <prethigah@gmail.com>
2026-01-04 22:13:47 +0530

    Update store to include ownerSlice

--- Client/src/redux/store.js
+++ Client/src/redux/store.js
@@ -1,9 +1,11 @@
 // BiteRoute / Client / src / redux / store.js
 import { configureStore } from "@reduxjs/toolkit";
 import userSlice from "./userSlice";
+import ownerSlice from "./ownerSlice";
 
 export const store = configureStore({
   reducer: {
     user: userSlice,
+    owner: ownerSlice,
   },
 });
