c7d5164ecd00f3f96d3c5acf79f225da28c6a451
PrethigahShanmugarajah <prethigah@gmail.com>
2026-01-04 21:24:20 +0530

    Update UserDashboard component

--- Client/src/components/Dashboards/UserDashboard.jsx
+++ Client/src/components/Dashboards/UserDashboard.jsx
@@ -1,9 +1,10 @@
 // BiteRoute / Client / src / components / Dashboards / UserDashboard.jsx
+import Nav from "../Nav";
 
 const UserDashboard = () => {
   return (
-    <div>
-      <h1>UserDashboard</h1>
+    <div className="w-full min-h-screen bg-bg flex flex-col items-center">
+      <Nav />
     </div>
   );
 };
