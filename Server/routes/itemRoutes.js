3b3168ee2813f2ba06031200a92108eb22b580df
PrethigahShanmugarajah <prethigah@gmail.com>
2026-01-04 19:19:11 +0530

    Add updateItem route in itemRoutes

--- Server/routes/itemRoutes.js
+++ Server/routes/itemRoutes.js
@@ -1,11 +1,17 @@
 // Cravely / Server / routes / itemRoutes.js
 import express from "express";
 import { isAuth } from "../middlewares/isAuth.js";
-import { addItem } from "../controllers/itemController.js";
+import { addItem, updateItem } from "../controllers/itemController.js";
 import upload from "../middlewares/multer.js";
 
 const itemRouter = express.Router();
 
 itemRouter.post("/add-item", isAuth, upload.single("image"), addItem);
+itemRouter.put(
+  "/update-item/:itemId",
+  isAuth,
+  upload.single("image"),
+  updateItem
+);
 
 export default itemRouter;
