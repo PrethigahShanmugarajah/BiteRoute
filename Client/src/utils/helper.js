fa9f82cd6c42162785119e01a0ed59071990ad8a
PrethigahShanmugarajah <prethigah@gmail.com>
2026-01-04 16:02:39 +0530

    Add capitalizeWords utility function

--- Client/src/utils/helper.js
+++ Client/src/utils/helper.js
@@ -5,3 +5,12 @@ export const capitalizeFirstLetter = (str) => {
   if (!str) return "";
   return str.charAt(0).toUpperCase() + str.slice(1);
 };
+
+/* -------- Capitalize first letter of every word -------- */
+export const capitalizeWords = (str) => {
+  if (!str) return "";
+  return str
+    .split(" ")
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ");
+};
