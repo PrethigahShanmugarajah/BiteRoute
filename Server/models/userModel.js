bc19f138f63a32b25ea3cc0f164e4dcc99e60eaf
PrethigahShanmugarajah <prethigah@gmail.com>
2026-01-04 15:56:04 +0530

    Update userModel

--- Server/models/userModel.js
+++ Server/models/userModel.js
@@ -21,7 +21,7 @@ const userSchema = new mongoose.Schema(
     },
     role: {
       type: String,
-      enum: ["user", "owner", "deliveryBoy"],
+      enum: ["user", "owner", "deliveryPerson"],
       required: true,
     },
     resetOtp: {
