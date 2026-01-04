606f8a87a97480b3ae22b0a35cf0f2e637bfa5ad
PrethigahShanmugarajah <prethigah@gmail.com>
2026-01-04 21:22:52 +0530

    Update useGetCurrentUser

--- Client/src/hooks/useGetCurrentUser.jsx
+++ Client/src/hooks/useGetCurrentUser.jsx
@@ -5,41 +5,30 @@ import API_ROUTES from "../api/api_route";
 import { toast } from "react-toastify";
 import { setUserData } from "../redux/userSlice";
 import { useDispatch } from "react-redux";
-import { fetchUser } from "../services/fetch";
 
 function useGetCurrentUser() {
-  // useEffect(() => {
-  //   const fetchUser = async () => {
-  //     try {
-  //       const { data } = await api.get(API_ROUTES.USER.USER_GET, {
-  //         withCredentials: true,
-  //       });
-  //       console.log("Fetch User API Response:", data);
-  //       if (data.success) {
-  //         console.log("Fetch User Success:", data.message);
-  //         dispatch(setUserData(data.user));
-  //       } else {
-  //         toast.error(data.message);
-  //         console.log("Fetch User Data Error:", error);
-  //       }
-  //     } catch (error) {
-  //       toast.error(error?.response?.data?.message || error?.message);
-  //       console.log("Fetch User Error:", error);
-  //     }
-  //     fetchUser();
-  //   };
-  // }, []);
-
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const fetchUserService = async () => {
-      const user = await fetchUser();
-      if (user) {
-        dispatch(setUserData(user));
+    const fetchUser = async () => {
+      try {
+        const { data } = await api.get(API_ROUTES.USER.USER_GET, {
+          withCredentials: true,
+        });
+        console.log("Fetch User API Response:", data);
+        if (data.success) {
+          console.log("Fetch User Success:", data.message);
+          dispatch(setUserData(data.user));
+        } else {
+          toast.error(data.message);
+          console.log("Fetch User Data Error:", error);
+        }
+      } catch (error) {
+        toast.error(error?.response?.data?.message || error?.message);
+        console.log("Fetch User Error:", error);
       }
     };
-    fetchUserService();
+    fetchUser();
   }, [dispatch]);
 }
 
