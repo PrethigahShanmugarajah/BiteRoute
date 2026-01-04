b1938e156e1ca06c96d94e5f2882caa7c9cf1a09
PrethigahShanmugarajah <prethigah@gmail.com>
2026-01-04 16:05:01 +0530

    Add Nav component to Home page

--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -3,15 +3,17 @@ import { useSelector } from "react-redux";
 import UserDashboard from "../components/Dashboards/UserDashboard";
 import OwnerDashboard from "../components/Dashboards/OwnerDashboard";
 import DeliveryPerson from "../components/Dashboards/DeliveryPerson";
+import Nav from "../components/Nav";
 
 const Home = () => {
   const { userData } = useSelector((state) => state.user);
 
   return (
     <div className="w-screen min-h-screen pt-25 flex flex-col items-center bg-bg">
+      <Nav />
       {userData.role == "user" && <UserDashboard />}
       {userData.role == "owner" && <OwnerDashboard />}
-      {userData.role == "deliveryBoy" && <DeliveryPerson />}
+      {userData.role == "deliveryPerson" && <DeliveryPerson />}
     </div>
   );
 };
