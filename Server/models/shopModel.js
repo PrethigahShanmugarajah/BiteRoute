358c210e28be1cebf602819a2e458397a866463c
PrethigahShanmugarajah <prethigah@gmail.com>
2026-01-04 16:54:27 +0530

    Add shopModel

--- Server/models/shopModel.js
+++ Server/models/shopModel.js
@@ -5,9 +5,13 @@ const shopSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
     image: { type: String, required: true },
-    owner: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-    city: { type: String, required: true },
-    state: { type: String, required: true },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    district: { type: String, required: true },
+    province: { type: String, required: true },
     address: { type: String, required: true },
     items: { type: mongoose.Schema.Types.ObjectId, ref: "Item" },
   },
