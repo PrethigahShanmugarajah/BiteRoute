2dcee8092a660ec798f4172ed836d5691e48db26
PrethigahShanmugarajah <prethigah@gmail.com>
2026-01-05 18:51:59 +0530

    Add getItemById route in itemRoutes

--- Server/routes/itemRoutes.js
+++ Server/routes/itemRoutes.js
@@ -1,7 +1,11 @@
 // Cravely / Server / routes / itemRoutes.js
 import express from "express";
 import { isAuth } from "../middlewares/isAuth.js";
-import { addItem, updateItem } from "../controllers/itemController.js";
+import {
+  addItem,
+  getItemById,
+  updateItem,
+} from "../controllers/itemController.js";
 import upload from "../middlewares/multer.js";
 
 const itemRouter = express.Router();
@@ -13,5 +17,6 @@ itemRouter.put(
   upload.single("image"),
   updateItem
 );
+itemRouter.get("/get-item/:itemId", isAuth, getItemById);
 
 export default itemRouter;
