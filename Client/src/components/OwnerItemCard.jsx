559425eecc0349c1843502dffa9fade321c4f163
PrethigahShanmugarajah <prethigah@gmail.com>
2026-01-06 00:05:20 +0530

    Add delete confirmation popup in OwnerItemCard

--- Client/src/components/OwnerItemCard.jsx
+++ Client/src/components/OwnerItemCard.jsx
@@ -1,4 +1,5 @@
 // BiteRoute / Client / src / components / OwnerItemCard.jsx
+import { useState } from "react";
 import { FaPen } from "react-icons/fa6";
 import { FaTrashAlt } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
@@ -8,13 +9,19 @@ import API_ROUTES from "../api/api_route";
 import { useDispatch } from "react-redux";
 import { setMyShopData } from "../redux/ownerSlice";
 import { toast } from "react-toastify";
+import DeletePopup from "./DeletePopup";
 
 const OwnerItemCard = ({ data }) => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
+  const [showDelete, setShowDelete] = useState(false);
+  const [loading, setLoading] = useState(false);
+
   const handleDeleteItem = async (itemId) => {
     try {
+      setLoading(true);
+
       const response = await api.delete(API_ROUTES.ITEM.ITEM_DELETE(itemId), {
         withCredentials: true,
       });
@@ -29,6 +36,8 @@ const OwnerItemCard = ({ data }) => {
 
         dispatch(setMyShopData(data.shop));
         console.log("Delete ItemDispatch:", data.shop);
+
+        setShowDelete(false);
       } else {
         toast.warn(data?.message);
         console.log("Delete Item Data Error:", data?.message);
@@ -36,6 +45,8 @@ const OwnerItemCard = ({ data }) => {
     } catch (error) {
       toast.error(error?.response?.data?.message || error?.message);
       console.log("Delete Item Error:", error);
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -77,11 +88,20 @@ const OwnerItemCard = ({ data }) => {
 
             <div
               className="p-2 rounded-full hover:bg-primary/10 text-hover cursor-pointer"
-              onClick={() => handleDeleteItem(data._id)}
+              // onClick={() => handleDeleteItem(data._id)}
+              onClick={() => setShowDelete(true)}
             >
               <FaTrashAlt size={16} />
             </div>
           </div>
+
+          {showDelete && (
+            <DeletePopup
+              onClose={() => setShowDelete(false)}
+              onDelete={() => handleDeleteItem(data._id)}
+              loading={loading}
+            />
+          )}
         </div>
       </div>
     </div>
