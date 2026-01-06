153f4578656e192ab6bca63caa46ee360f9637d4
PrethigahShanmugarajah <prethigah@gmail.com>
2026-01-06 15:41:30 +0530

    Add food categories with images

---
+++ Client/src/category.js
@@ -0,0 +1,28 @@
+// BiteRoute / Client / src / category.js
+import Breakfast from "./assets/Breakfast.png";
+import Lunch from "./assets/Lunch.png";
+import Dinner from "./assets/Dinner.png";
+import Short_Eats from "./assets/Short_Eats.png";
+import Kottu from "./assets/Kottu.png";
+import Noodles from "./assets/Noodles.png";
+import Pizza from "./assets/Pizza.png";
+import Seafood from "./assets/Seafood.png";
+import Desserts_and_Sweets from "./assets/Desserts_and_Sweets.png";
+import Burgers from "./assets/Burgers.png";
+import Sandwiches from "./assets/Sandwiches.png";
+import Others from "./assets/Others.png";
+
+export const categories = [
+  { category: "Breakfast", image: Breakfast },
+  { category: "Lunch", image: Lunch },
+  { category: "Dinner", image: Dinner },
+  { category: "Short Eats", image: Short_Eats },
+  { category: "Kottu", image: Kottu },
+  { category: "Noodles", image: Noodles },
+  { category: "Pizza", image: Pizza },
+  { category: "Seafood", image: Seafood },
+  { category: "Desserts and Sweets", image: Desserts_and_Sweets },
+  { category: "Burgers", image: Burgers },
+  { category: "Sandwiches", image: Sandwiches },
+  { category: "Others", image: Others },
+];
