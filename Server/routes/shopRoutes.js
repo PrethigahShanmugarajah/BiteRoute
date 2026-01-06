629b3ea58342f728cae2788266038c439bdf0698
PrethigahShanmugarajah <prethigah@gmail.com>
2026-01-06 21:03:32 +0530

    Add getShopByCity route in shopRoutes

--- Server/routes/shopRoutes.js
+++ Server/routes/shopRoutes.js
@@ -1,12 +1,17 @@
 // Cravely / Server / routes / shopRoutes.js
 import express from "express";
 import { isAuth } from "../middlewares/isAuth.js";
-import { createEditShop, getMyShop } from "../controllers/shopController.js";
+import {
+  createEditShop,
+  getMyShop,
+  getShopByCity,
+} from "../controllers/shopController.js";
 import upload from "../middlewares/multer.js";
 
 const shopRouter = express.Router();
 
 shopRouter.post("/create-edit", isAuth, upload.single("image"), createEditShop);
 shopRouter.get("/get-shop", isAuth, getMyShop);
+shopRouter.get("/get-shop-by-city/:city", isAuth, getShopByCity);
 
 export default shopRouter;
