3282f13fbb4782aed429028719ebe8164d88411b
PrethigahShanmugarajah <prethigah@gmail.com>
2026-01-09 02:34:55 +0530

    Add route for getUserOrders in orderRoutes

--- Server/routes/orderRoutes.js
+++ Server/routes/orderRoutes.js
@@ -1,10 +1,11 @@
 // Cravely / Server / routes / orderRoutes.js
 import express from "express";
 import { isAuth } from "../middlewares/isAuth.js";
-import { placeOrder } from "../controllers/orderController.js";
+import { getUserOrders, placeOrder } from "../controllers/orderController.js";
 
 const orderRouter = express.Router();
 
 orderRouter.post("/place-order", isAuth, placeOrder);
+orderRouter.get("/user-orders", isAuth, getUserOrders);
 
 export default orderRouter;
