b5464e60ac5a1a72feb1dc0737a7e631436b3b9f
PrethigahShanmugarajah <prethigah@gmail.com>
2026-01-09 00:28:38 +0530

    Add orderRouter

--- Server/server.js
+++ Server/server.js
@@ -8,6 +8,7 @@ import authRouter from "./routes/authRoutes.js";
 import userRouter from "./routes/userRoutes.js";
 import shopRouter from "./routes/shopRoutes.js";
 import itemRouter from "./routes/itemRoutes.js";
+import orderRouter from "./routes/orderRoutes.js";
 
 /* -------- INITIALIZE EXPRESS -------- */
 const app = express();
@@ -31,6 +32,7 @@ app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
 app.use("/api/shop", shopRouter);
 app.use("/api/item", itemRouter);
+app.use("/api/order", orderRouter);
 
 /* -------- PORT -------- */
 const port = process.env.PORT || 5000;
