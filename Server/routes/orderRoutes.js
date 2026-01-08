d10b74f58adf1b4be8595145e6e211ea73536460
PrethigahShanmugarajah <prethigah@gmail.com>
2026-01-09 00:27:24 +0530

    Add route for placeOrder in orderRoutes

---
+++ Server/routes/orderRoutes.js
@@ -0,0 +1,10 @@
+// Cravely / Server / routes / orderRoutes.js
+import express from "express";
+import { isAuth } from "../middlewares/isAuth.js";
+import { placeOrder } from "../controllers/orderController.js";
+
+const orderRouter = express.Router();
+
+orderRouter.post("/place-order", isAuth, placeOrder);
+
+export default orderRouter;
