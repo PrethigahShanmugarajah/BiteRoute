aec531611f4637d697fa5931759f05385eb0ba03
PrethigahShanmugarajah <prethigah@gmail.com>
2026-01-17 03:34:35 +0530

    Create useGetMyOrders hook to fetch and set user orders

---
+++ Client/src/hooks/useGetMyOrders.jsx
@@ -0,0 +1,42 @@
+// BiteRoute / Client / src / hooks / useGetMyOrders.jsx
+import { useEffect } from "react";
+import api from "../api/axios";
+import API_ROUTES from "../api/api_route";
+import { toast } from "react-toastify";
+import { useDispatch, useSelector } from "react-redux";
+import { setMyOrders } from "../redux/userSlice";
+
+function useGetMyOrders() {
+  const dispatch = useDispatch();
+  const { userData } = useSelector((state) => state.user);
+
+  useEffect(() => {
+    if (!userData) return;
+
+    const fetchMyOrders = async () => {
+      try {
+        const { data } = await api.get(API_ROUTES.ORDER.ORDER_MY_GET, {
+          withCredentials: true,
+        });
+
+        console.log("Fetch Order API Response:", data);
+
+        if (data.success) {
+          console.log("Fetch Order Success:", data.message);
+          dispatch(setMyOrders(data.orders));
+
+          console.log("Dispatch Orders:", data.orders);
+        } else {
+          toast.error(data.message);
+          console.log("Fetch Order Data Error:", data.message);
+        }
+      } catch (error) {
+        toast.error(error?.response?.data?.message || error?.message);
+        console.log("Fetch Order Error:", error);
+      }
+    };
+    fetchMyOrders();
+  }, [dispatch, userData]);
+}
+
+export default useGetMyOrders;
