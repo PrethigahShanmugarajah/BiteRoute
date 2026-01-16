7e94070c73ffac1fff9b568b8808a35e6c358ffd
PrethigahShanmugarajah <prethigah@gmail.com>
2026-01-17 03:45:18 +0530

    Add UserOrderCard component to display user orders with details

---
+++ Client/src/components/UserOrderCard.jsx
@@ -0,0 +1,74 @@
+// BiteRoute / Client / src / components / UserOrderCard.jsx
+import {
+  capitalizeAll,
+  capitalizeFirstLetter,
+  formatDate,
+} from "../utils/helper";
+import Button from "./Button";
+
+const UserOrderCard = ({ data }) => {
+  return (
+    <div className="bg-white rounded-lg shadow p-4 space-y-4">
+      <div className="flex justify-between border-b pb-2 border-gray-300">
+        <div>
+          <p className="font-semibold">Order #{data._id.slice(-6)}</p>
+          <p className="text-sm text-gray-500">
+            Date: {formatDate(data.createdAt)}
+          </p>
+        </div>
+
+        <div className="text-right">
+          <p className="text-sm text-gray-500">
+            {capitalizeAll(data.paymentMethod)}
+          </p>
+          <p className="font-medium text-blue-600">
+            {capitalizeFirstLetter(data.shopOrders?.[0].status)}
+          </p>
+        </div>
+      </div>
+
+      {data.shopOrders.map((shopOrder, index) => (
+        <div
+          className="border border-gray-300 rounded-lg p-3 bg-bg space-y-3"
+          key={index}
+        >
+          <p>{shopOrder.shop.name}</p>
+
+          <div className="flex space-x-4 overflow-x-auto mb-2">
+            {shopOrder.shopOrderItems.map((item, index) => (
+              <div
+                key={index}
+                className="shrink-0 w-40 border border-gray-300 rounded-lg p-2 bg-white"
+              >
+                <img
+                  src={item.item.image}
+                  alt=""
+                  className="w-full h-24 object-cover rounded"
+                />
+
+                <p className="text-sm font-semibold mt-1">{item.name}</p>
+                <p className="text-xs text-gray-500">
+                  Qty: {item.quantity}x LKR {item.price}
+                </p>
+              </div>
+            ))}
+          </div>
+
+          <div className="flex justify-between items-center border-t border-gray-300 pt-2">
+            <p className="font-semibold">Subtotal: LKR {shopOrder.subtotal}</p>
+            <span className="text-sm font-medium text-blue-600">
+              {capitalizeFirstLetter(shopOrder.status)}
+            </span>
+          </div>
+        </div>
+      ))}
+
+      <div className="flex justify-between items-center border-t border-gray-300 pt-2">
+        <p className="font-semibold">Total: {data.totalAmount}</p>
+        <Button className="px-4! text-sm">Track Order</Button>
+      </div>
+    </div>
+  );
+};
+
+export default UserOrderCard;
