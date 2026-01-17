35082622cf0842fc835910c05fdf403c145d7982
PrethigahShanmugarajah <prethigah@gmail.com>
2026-01-17 22:10:41 +0530

    Add updateOrderStatus route in orderRoutes

--- Server/routes/orderRoutes.js
+++ Server/routes/orderRoutes.js
@@ -1,11 +1,16 @@
 // BiteRoute / Server / routes / orderRoutes.js
 import express from "express";
 import { isAuth } from "../middlewares/isAuth.js";
-import { getMyOrders, placeOrder } from "../controllers/orderController.js";
+import {
+  getMyOrders,
+  placeOrder,
+  updateOrderStatus,
+} from "../controllers/orderController.js";
 
 const orderRouter = express.Router();
 
 orderRouter.post("/place-order", isAuth, placeOrder);
 orderRouter.get("/my-orders", isAuth, getMyOrders);
+orderRouter.post("/update-status/:orderId/:shopId", isAuth, updateOrderStatus);
 
 export default orderRouter;
