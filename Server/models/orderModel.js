a0f9eb6dd09533b9a1a34654d49621986f0a86a7
PrethigahShanmugarajah <prethigah@gmail.com>
2026-01-18 02:25:34 +0530

    Add delivery assignment and assigned delivery person fields to order model

--- Server/models/orderModel.js
+++ Server/models/orderModel.js
@@ -3,7 +3,11 @@ import mongoose from "mongoose";
 
 const shopOrderItemsSchema = new mongoose.Schema(
   {
-    item: { type: mongoose.Schema.Types.ObjectId, ref: "Item", required: true },
+    item: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Item",
+      required: true,
+    },
     name: String,
     price: Number,
     quantity: Number,
@@ -13,8 +17,14 @@ const shopOrderItemsSchema = new mongoose.Schema(
 
 const shopOrderSchema = new mongoose.Schema(
   {
-    shop: { type: mongoose.Schema.Types.ObjectId, ref: "Shop" },
-    owner: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    shop: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Shop",
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
     subtotal: Number,
     shopOrderItems: [shopOrderItemsSchema],
     status: {
@@ -22,16 +32,38 @@ const shopOrderSchema = new mongoose.Schema(
       enum: ["pending", "preparing", "out of delivery", "delivered"],
       default: "pending",
     },
+    assignment: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "DeliveryAssignment",
+      default: null,
+    },
+    assignedDeliveryPerson: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
   },
   { timestamps: true }
 );
 
 const orderSchema = new mongoose.Schema(
   {
-    user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-    paymentMethod: { type: String, enum: ["cod", "online"], required: true },
-    deliveryAddress: { text: String, latitude: Number, longitude: Number },
-    totalAmount: { type: Number },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    paymentMethod: {
+      type: String,
+      enum: ["cod", "online"],
+      required: true,
+    },
+    deliveryAddress: {
+      text: String,
+      latitude: Number,
+      longitude: Number,
+    },
+    totalAmount: {
+      type: Number,
+    },
     shopOrders: [shopOrderSchema],
   },
   { timestamps: true }
