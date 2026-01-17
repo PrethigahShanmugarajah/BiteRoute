22ac9e05d8a8a3b016624aa723489cac0676bf8b
PrethigahShanmugarajah <prethigah@gmail.com>
2026-01-18 02:23:28 +0530

    Add updateUserLocation route in userRoutes

--- Server/routes/userRoutes.js
+++ Server/routes/userRoutes.js
@@ -1,10 +1,14 @@
 // Cravely / Server / routes / userRoutes.js
 import express from "express";
 import { isAuth } from "../middlewares/isAuth.js";
-import { getCurrentUser } from "../controllers/userController.js";
+import {
+  getCurrentUser,
+  updateUserLocation,
+} from "../controllers/userController.js";
 
 const userRouter = express.Router();
 
 userRouter.get("/userget", isAuth, getCurrentUser);
+userRouter.post("/update-location", isAuth, updateUserLocation);
 
 export default userRouter;
