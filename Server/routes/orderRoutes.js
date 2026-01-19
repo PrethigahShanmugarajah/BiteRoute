87eedaf25991a4688cce02ecf4becc2017bac751
PrethigahShanmugarajah <prethigah@gmail.com>
2026-01-20 02:12:38 +0530

    Add getCurrentOrder route in orderRoutes

--- Server/routes/orderRoutes.js
+++ Server/routes/orderRoutes.js
@@ -3,6 +3,7 @@ import express from "express";
 import { isAuth } from "../middlewares/isAuth.js";
 import {
   acceptOrder,
+  getCurrentOrder,
   getDeliveryPersonAssignment,
   getMyOrders,
   placeOrder,
@@ -16,5 +17,6 @@ orderRouter.get("/my-orders", isAuth, getMyOrders);
 orderRouter.post("/update-status/:orderId/:shopId", isAuth, updateOrderStatus);
 orderRouter.get("/get-assignments", isAuth, getDeliveryPersonAssignment);
 orderRouter.post("/accept-order/:assignmentId", isAuth, acceptOrder);
+orderRouter.get("/get-current-order", isAuth, getCurrentOrder);
 
 export default orderRouter;
