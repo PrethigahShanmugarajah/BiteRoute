ae53a7bf11770f388b91622ba5c373877f61cac4
PrethigahShanmugarajah <prethigah@gmail.com>
2026-01-20 02:48:48 +0530

    Add DeliveryPersonTracking component

---
+++ Client/src/components/DeliveryPersonTracking.jsx
@@ -0,0 +1,11 @@
+// BiteRoute / Client / src / components / DeliveryPersonTracking.jsx
+
+const DeliveryPersonTracking = () => {
+  return (
+    <div>
+      <h1>DeliveryPersonTracking</h1>
+    </div>
+  );
+};
+
+export default DeliveryPersonTracking;
