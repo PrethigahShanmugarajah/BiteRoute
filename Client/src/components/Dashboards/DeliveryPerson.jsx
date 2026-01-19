47b79be55c7052c1919b79cf13cc1ff8d8da72e5
PrethigahShanmugarajah <prethigah@gmail.com>
2026-01-20 01:19:01 +0530

    Add DeliveryPerson dashboard with available assignments and accept order functionality

--- Client/src/components/Dashboards/DeliveryPerson.jsx
+++ Client/src/components/Dashboards/DeliveryPerson.jsx
@@ -1,9 +1,122 @@
 // BiteRoute / Client / src / components / Dashboards / DeliveryPerson.jsx
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import Nav from "../Nav";
+import api from "../../api/axios";
+import API_ROUTES from "../../api/api_route";
+import Button from "../Button";
+import { capitalizeWords } from "../../utils/helper";
+import { toast } from "react-toastify";
 
 const DeliveryPerson = () => {
+  const { userData } = useSelector((state) => state.user);
+  const [availableAssignments, setAvailableAssignments] = useState([]);
+
+  const getAssignments = async () => {
+    try {
+      const { data } = await api.get(
+        API_ROUTES.ORDER.ORDER_GET_DELIVERYPERSON_ASSIGNMENTS,
+        { withCredentials: true }
+      );
+
+      console.log("Fetch Assignments API Response:", data);
+
+      if (data.success) {
+        setAvailableAssignments(data.formated);
+        console.log("Fetch Assignments Success:", data.message);
+      } else {
+        toast.error(data.message);
+        console.log("Fetch Assignments Data Error:", data.message);
+      }
+    } catch (error) {
+      toast.error(error?.response?.data?.message || error?.message);
+      console.log("Fetch Assignments Error:", error);
+    }
+  };
+
+  const acceptOrder = async (assignmentId) => {
+    try {
+      const { data } = await api.post(
+        API_ROUTES.ORDER.ORDER_ACCEPT(assignmentId),
+        {},
+        { withCredentials: true }
+      );
+
+      console.log("Accept Order API Response:", data);
+
+      if (data.success) {
+        toast.success(data.message);
+        console.log("Accept Order Success:", data.message);
+
+        getAssignments();
+      } else {
+        toast.error(data.message);
+        console.log("Accept Order Data Error:", data.message);
+      }
+    } catch (error) {
+      toast.error(error?.response?.data?.message || error?.message);
+      console.log("Accept Order Error:", error);
+    }
+  };
+
+  useEffect(() => {
+    getAssignments();
+  }, [userData]);
+
   return (
-    <div>
-      <h1>DeliveryPerson</h1>
+    <div className="w-screen min-h-screen flex flex-col gap-5 items-center bg-bg overflow-y-auto">
+      <Nav />
+      <div className="w-full max-w-200 flex flex-col gap-5 items-center">
+        <div className="bg-white rounded-2xl shadow-md p-5 flex flex-col justify-start items-center w-[90%] border border-gray-300 text-center gap-4">
+          <h1 className="text-xl font-bold text-primary">
+            Welcome, {capitalizeWords(userData?.fullName)}
+          </h1>
+
+          <p className="text-gray-500">
+            <span className="font-semibold text-black">Latitude:</span>
+            {userData?.location?.coordinates[1]},{" "}
+            <span className="font-semibold text-black">Longitude:</span>
+            {userData?.location?.coordinates[0]}
+          </p>
+        </div>
+
+        <div className="bg-white rounded-2xl p-5 shadow-md w-[90%] border border-gray-300">
+          <h1 className="text-lg font-bold mb-4 flex items-center gap-2">
+            Available Orders
+          </h1>
+
+          <div className="space-y-4">
+            {availableAssignments?.length > 0 ? (
+              availableAssignments?.map((a, index) => (
+                <div
+                  className="border border-gray-300 rounded-lg p-4 flex justify-between items-center"
+                  key={index}
+                >
+                  <div>
+                    <p className="text-sm font-semibold">{a?.shopName}</p>
+                    <p className="text-sm text-gray-500">
+                      <span className="font-semibold">Delivery Address:</span>{" "}
+                      {a?.deliveryAddress.text}
+                    </p>
+                    <p className="text-xs text-gray-500">
+                      {a?.items?.length} items | {a?.subtotal}
+                    </p>
+                  </div>
+
+                  <Button
+                    className="px-4! py-1! text-sm"
+                    onClick={() => acceptOrder(a.assignmentId)}
+                  >
+                    Accept
+                  </Button>
+                </div>
+              ))
+            ) : (
+              <p className="text-gray-500 text-sm">No Available Orders</p>
+            )}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
