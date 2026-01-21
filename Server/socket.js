31eddc2e7ea12b21dad19dfbc18062e845c98d06
PrethigahShanmugarajah <prethigah@gmail.com>
2026-01-22 02:51:07 +0530

    Enhance socket handler to track user identity, socket ID, and online status

--- Server/socket.js
+++ Server/socket.js
@@ -1,6 +1,47 @@
 // BiteRoute / Server / socket.js
+import User from "./models/userModel.js";
+
 export const socketHandler = async (io) => {
   io.on("connection", (socket) => {
-    console.log("Socket ID:", socket.id);
+    socket.on("identity", async ({ userId }) => {
+      try {
+        const user = await User.findByIdAndUpdate(
+          userId,
+          { socketId: socket.id, isOnline: true },
+          { new: true },
+        );
+      } catch (error) {
+        console.error("Socket Handle Error:", error.message);
+
+        return res.status(500).json({
+          success: false,
+          message: "",
+          error: `Socket Handle Error: ${error.message}`,
+        });
+      }
+
+      console.log("Socket ID:", socket.id);
+      console.log("Socket Indentity:", { userId });
+    });
+
+    socket.on("disconnect", async () => {
+      try {
+        await User.findOneAndUpdate(
+          { socketId: socket.id },
+          {
+            socketId: null,
+            isOnline: false,
+          },
+        );
+      } catch (error) {
+        console.error("Socket Handle Error:", error.message);
+
+        return res.status(500).json({
+          success: false,
+          message: "",
+          error: `Socket Handle Error: ${error.message}`,
+        });
+      }
+    });
   });
 };
